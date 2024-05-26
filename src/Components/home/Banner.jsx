89421c928f2fb5e368b05df4539567d4db4cb788
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2024-05-26 11:11:51 +0600

    navbar updated

---
+++ src/Components/home/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+    return (
+        <div>
+            <div className='bg-no-repeat bg-cover h-screen' style={{ backgroundImage: `url("https://img.freepik.com/premium-photo/spicy-seafood-salad-with-fresh-shrimps_73523-1882.jpg?w=740")` }}>
+            <h1 className='text-7xl text-center text-white'>Find your favourite food</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Banner
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../Components/home/NavBar'
+import Banner from '../Components/home/Banner'
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <Banner />
     </div>
   )
 }
