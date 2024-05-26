55c1d8474fa077b91b980e59bca906c055716c59
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2024-05-26 11:33:50 +0600

    create banner

--- src/Components/home/Banner.jsx
+++ src/Components/home/Banner.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 
 const Banner = () => {
     return (
-        <div>
-            <div className='bg-no-repeat bg-cover h-screen' style={{ backgroundImage: `url("https://img.freepik.com/premium-photo/spicy-seafood-salad-with-fresh-shrimps_73523-1882.jpg?w=740")` }}>
-            <h1 className='text-7xl text-center text-white'>Find your favourite food</h1>
-            </div>
+        <div
+      className="hero min-h-[400px] "
+      style={{
+        backgroundImage: `url("https://img.freepik.com/free-photo/dinner-preparation-with-uncooked-pastas-cayenne-peppers-tied-one-another-with-rope-oil-bottle-lemon-garlic-notebook-mixed-color-table_140725-86664.jpg?t=st=1716691802~exp=1716695402~hmac=880f1ede8d0443d324ca1caf8564d3e783c7594f75f1039e6b8789ef56bc42b4&w=740")`,
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-start text-neutral-content">
+        <div className="max-w-md text-center">
+          <h1 className="mb-5 text-5xl font-serif font-semibold">Find your Favourite Food</h1>
+          
+          <button className="btn btn-primary">Get Started</button>
         </div>
+      </div>
+    </div>
     )
 }
 
