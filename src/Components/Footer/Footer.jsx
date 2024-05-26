d99f9df0d0624dfde06d3603e90aa71ef364eb0a
Chamak Karmakar <chamakkarmakar.ck@gmail.com>
2024-05-26 11:53:53 +0600

    added footer

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    
+    <footer className="flex flex-col text-black">
+    <div className="flex flex-col items-start justify-around gap-5 bg-sky-50 p-10 py-10 md:flex-row md:gap-0 md:items-center">
+        <aside className="text-xl">
+           
+            <p className='text-4xl font-semibold font-serif'>Recipe</p>
+        </aside>
+        <nav className="text-lg">
+            <ul className="space-y-3">
+                <li>
+                    <a className="cursor-pointer hover:underline">Home</a>
+                </li>
+                <li>
+                    <a className="cursor-pointer hover:underline">Contact</a>
+                </li>
+                <li>
+                    <a className="cursor-pointer hover:underline">About</a>
+                </li>
+            </ul>
+        </nav>
+        <nav className="text-lg">
+            <ul className="space-y-3">
+                <li>
+                    <a className="cursor-pointer hover:underline">Services</a>
+                </li>
+                <li>
+                    <a className="cursor-pointer hover:underline">Add</a>
+                </li>
+                
+            </ul>
+        </nav>
+    </div>
+    <aside className="bg-sky-100 py-5 text-center text-sm">
+        <p>&copy; 2024 Recipe. All Rights Reserved.</p>
+    </aside>
+</footer>
+
+  )
+}
+
+export default Footer
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import NavBar from '../Components/home/NavBar'
 import Banner from '../Components/home/Banner'
 
 const Home = () => {
   return (
     <div>
-      <NavBar />
       <Banner />
     </div>
   )
